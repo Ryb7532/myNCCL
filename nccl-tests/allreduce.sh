9bb1f2488b72189bf6d91cf6225c5ab3e8a6effe
HosokiRyubu <hosoki.r.aa@m.titech.ac.jp>
2022-01-18 09:42:04 +0900

    add results under different configs

--- nccl-tests/allreduce.sh
+++ nccl-tests/allreduce.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$ -cwd
-#$ -l f_node=2
+#$ -l f_node=8
 #$ -l h_rt=00:10:00
 #$ -N allreduce
 
@@ -9,6 +9,5 @@ module load gcc/8.3.0 intel cuda/11.2.146 openmpi nccl
 
 #export NCCL_DEBUG=INFO
 export NCCL_BUFFSIZE=524288
-export NCCL_RINGS="0 1 2 3 | 3 2 1 0"
 
-mpirun -x LD_LIBRARY_PATH -x PATH -np 2 -npernode 1 --bind-to none ./build/all_reduce_perf -b 4K -e 4096M -f 4 -g 2
+mpirun -x LD_LIBRARY_PATH -x PATH -np 8 -npernode 1 --bind-to none ./build/all_reduce_perf -b 4K -e 4096M -f 4 -g 3
